b9e1bc4b7d906dfc1e9c7d60a139a7bd8c040a7c
qer <wbxl2000@outlook.com>
2020-08-11 10:55:55 +0800

    11 add static web server

--- 11 app.get/app.js
+++ 11 app.get/app.js
@@ -5,6 +5,9 @@ const ejs = require('ejs');
 // 注册 web 服务
 http.createServer(app).listen(8081);
 
+// 配置静态web服务
+app.static('public');
+
 // 配置路由
 app.get('/', function(req, res) {
     res.send('首页');
--- 11 app.get/module/routes.js
+++ 11 app.get/module/routes.js
@@ -1,5 +1,8 @@
 const url = require('url');
+const fs = require('fs');
+const path = require('path');
 
+// 扩展res
 function changeRes(res) {
     res.send = (data) => {
         res.writeHead(200, { 'Content-Type': 'text/html;charset="utf-8"' });
@@ -7,15 +10,50 @@ function changeRes(res) {
     }
 }
 
+// 根据后缀名获取文件没醒
+function getFileMime(extname) {
+    let data = fs.readFileSync('./data/mime.json');
+    let mimeObj = JSON.parse(data.toString());
+    return mimeObj[extname];
+}
+
+// 静态web服务方法
+function initStatic(req, res, staticPath) {
+    // 1. 获取地址
+    let pathname = url.parse(req.url).pathname;
+    pathname = pathname=='/'?'/index.html':pathname;
+    let extname = path.extname(pathname);
+    // 2. 通过fs模块读取文件
+    // 同步方法
+    try {
+        let data = fs.readFileSync('./' + staticPath + pathname);
+        if (data) {
+            let mime = getFileMime(extname);
+            // console.log('  -' + extname + ' ' + mime);
+            res.writeHead(200, { 'Content-Type': '' + mime + ';charset="utf-8"' });
+            res.end(data);
+        }
+    } catch (err) {
+
+    }
+}
+
 let server =  () => {
-    let G = {};
-    // 区分 get 和 post
-    G._get = {};
-    G._post = {};
+    let G = {
+        // 区分 get 和 post
+        _get:{},
+        _post:{},
+        // 默认路径 静态web目录
+        staticPath: 'static'
+    };
 
     let app = function(req, res) {
         // 扩展res的方法
         changeRes(res);
+
+        // 配置静态web服务
+        initStatic(req, res, G.staticPath);
+
 
         let pathname = url.parse(req.url).pathname;
         // 获取请求类型
@@ -50,6 +88,11 @@ let server =  () => {
         G._post[str] = cb;
         console.log(str);
         console.log(G._post['/doLogin']);
+    }
+
+    // 配置静态web服务目录
+    app.static = function(staticPath) {
+        G.staticPath = staticPath;
     }
 
     return app;
